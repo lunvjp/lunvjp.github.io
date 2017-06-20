f25f693b0b8d97f89ada861eb37e2b2f9c292bf7
lunvjp <momabz6@gmail.com>
2017-06-20 15:43:24 +0700

    Add files via upload

--- js/action.js
+++ js/action.js
@@ -1,4 +1,4 @@
-var dem = 0;
+var dem;
 var days = 3650;
 
 function daysLimit(days) {
@@ -8,6 +8,34 @@ function daysLimit(days) {
     return expires;
 }
 
+function TaoDanhSach(count) {
+    var temp = count.toString();
+    document.cookie = "AccountNumbers=" + temp + ";expires=" + daysLimit(days) + ";domain=.lunvjp.tk;path=/";
+}
+
+function IndexOfItem() {
+    var cookies = document.cookie;
+    var accountList = cookies.split("; ");
+
+    var index;
+    for (var i = 0; i <= accountList.length; i++) {
+        index = accountList[i].split("=");
+        if (index[0] === "AccountNumbers") {
+            return index[1];
+        }
+    }
+}
+
+function reLoad() {
+
+    if (document.cookie.length === 0) {
+        TaoDanhSach(0);
+        dem = 0;
+    } else {
+        dem = IndexOfItem();
+    }
+}
+
 function KiemTraTaiKhoan(id, pass) {
     var check = 3;
     if (document.cookie.length > 0) {
@@ -15,7 +43,6 @@ function KiemTraTaiKhoan(id, pass) {
         var accountList = temp.split(";");
         var taikhoan, matkhau;
         /* 1: Accepted, 2: Wrong Password, 3: Don't have ID */
-
         for (var i = 0; i < accountList.length; i++) {
             var cookies = accountList[i];
             var arrayCookie = cookies.split("=")[1];
@@ -41,6 +68,7 @@ function KiemTraTaiKhoan(id, pass) {
 }
 
 function checkAcount() {
+    reLoad();
     var id = $("#id").val();
     var pass = $("#login-password").val();
 
@@ -49,8 +77,6 @@ function checkAcount() {
     if (check === 1) {
         alert("Accepted");
         return true;
-
-
     } else if (check === 2) {
         alert("Wrong password!");
         return false;
@@ -84,9 +110,11 @@ function createAccount() {
         if (KiemTraTaiKhoan(email, pass) !== 3) {
             alert("That username is taken. Try another.");
         } else {
+            reLoad();
             dem++;
+            TaoDanhSach(dem);
             var phu = dem.toString();
-            document.cookie = "Account" + phu + "=" + "firstName: " + firstName + ",lastName: " + lastName + ",email: " + email + ",pass: " + pass + ",phone: " + phone + ";expires=" + daysLimit(days);
+            document.cookie = "Account" + phu + "=" + "firstName: " + firstName + ",lastName: " + lastName + ",email: " + email + ",pass: " + pass + ",phone: " + phone + ";expires=" + daysLimit(days) + ";domain=.lunvjp.tk;path=/";
             alert("Sign in succesfully");
         }
     }
