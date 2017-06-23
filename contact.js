df702f3d08f6a9a064a43dd4b399813534da4939
lunvjp <momabz6@gmail.com>
2017-06-23 11:31:28 +0700

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-<!-- optional, prevents the submit button from being pressed more than once -->
-var submitButton = document.getElementById("submit_form");
-var form = document.getElementById("email_form");
-form.addEventListener("submit", function (e) {
-    setTimeout(function () {
-        submitButton.value = "Sending...";
-        submitButton.disabled = true;
-    }, 1);
-});
